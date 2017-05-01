b360fec6cc905008197ca423421dcf2e57657ef8
West, John <john.west@tiaa.org>
2017-05-01 16:40:41 -0400

    added banker mode and fixed a few css issues

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
+app.get('/banker', (req, res) => {
+	res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
 app.get('/reset', (req, res) => {
 	res.sendFile(path.join(__dirname + '/public/reset.html'));
 });
